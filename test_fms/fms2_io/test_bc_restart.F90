2c6b118e8174b20d328f480260e8f4274d36e817
Uriel Ramirez <uramirez8707@gmail.com>
2020-10-30 13:51:31 -0400

    wut

--- test_fms/fms2_io/test_bc_restart.F90
+++ test_fms/fms2_io/test_bc_restart.F90
@@ -73,7 +73,7 @@ atm%BCfile_ne_open = .false.
 
 !< Try to write a BC restart file:
 atm%BCfile_sw_open = open_file(atm%fileobj_sw, "BCfile_sw.nc", "overwrite", is_restart=.true., pelist=all_pelist)
-atm%BCfile_ne_open = open_file(atm%fileobj_ne, "BCfile_ne.nc", "overwrite", is_restart=.true., pelist=all_pelist)
+!atm%BCfile_ne_open = open_file(atm%fileobj_ne, "BCfile_ne.nc", "overwrite", is_restart=.true., pelist=all_pelist)
 
 call register_bcs_2d(atm, atm%fileobj_ne, atm%fileobj_sw, "sst", layout)
 
@@ -97,7 +97,7 @@ atm%BCfile_ne_open = .false.
 
 !< Try to read the boundary condition restart back
 atm%BCfile_sw_open = open_file(atm%fileobj_sw, "BCfile_sw.nc", "read", is_restart=.true., pelist=all_pelist)
-atm%BCfile_ne_open = open_file(atm%fileobj_ne, "BCfile_ne.nc", "read", is_restart=.true., pelist=all_pelist)
+!atm%BCfile_ne_open = open_file(atm%fileobj_ne, "BCfile_ne.nc", "read", is_restart=.true., pelist=all_pelist)
 
 call register_bcs_2d(atm, atm%fileobj_ne, atm%fileobj_sw, "sst", layout)
 
