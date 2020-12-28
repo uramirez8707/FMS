ec4eb8e72eb416788998f08e5622f6c9bda09fd5
Uriel Ramirez <uriel.ramirez@noaa.gov>
2020-12-28 17:55:46 +0000

    Adds error checking in test_register_axis.F90

--- test_fms/fms2_io/test_register_axis.F90
+++ test_fms/fms2_io/test_register_axis.F90
@@ -17,9 +17,16 @@
 !* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
 
 program test_register_axis
-!> @brief  This programs tests register_axis functionality
+!> @brief  This programs tests functionality of:
+!!         register_axis (netcdf_add_dimension) interface
+!!         get_num_dimensions
+!!         get_dimension_names
+!!         get_unlimited_dimension_name
+!!         get_dimension_size
 use   fms2_io_mod,     only: open_file, close_file, FmsNetcdfFile_t, &
-                             register_axis, unlimited
+                             register_axis, unlimited, get_num_dimensions, &
+                             get_dimension_names, get_unlimited_dimension_name, &
+                             get_dimension_size
 use   fms_mod,         only: fms_init, fms_end
 use   mpp_mod,         only: mpp_npes, mpp_get_current_pelist, mpp_pe, &
                              mpp_error, FATAL
@@ -29,7 +36,10 @@ type(FmsNetcdfFile_t)              :: fileobj         !< fms2io fileobj
 integer                            :: dimlength       !< dimension length
 integer                            :: dimlength_in    !< dimension length read in
 integer, allocatable, dimension(:) :: all_pelist      !< list of all the pes
+integer                            :: ndims           !< number of dimension read in
 character(len=20), allocatable     :: dimnames(:)     !< dimension names read in
+character(len=20)                  :: unlimit_dimname !< unlimited dimension name read in
+integer                            :: dim_size        !< size of dimension read in
 
 call fms_init
 
@@ -60,7 +70,31 @@ if (open_file(fileobj, "test_register_axis.nc", "read", pelist=all_pelist)) then
    if (ndims .ne. 3) call mpp_error(FATAL, "The number of dimensions is not correct")
 
    !< Get the dimension names
+   allocate(dimnames(ndims))
    call get_dimension_names(fileobj, dimnames)
+   if (trim(dimnames(1)) .ne. "dim1") &
+       call mpp_error(FATAL, "dimnames(1) is not correct: "//trim(dimnames(1))//"")
+
+   if (trim(dimnames(2)) .ne. "dim2") &
+       call mpp_error(FATAL, "dimnames(2) is not correct: "//trim(dimnames(2))//"")
+
+   if (trim(dimnames(3)) .ne. "dim3") &
+       call mpp_error(FATAL, "dimnames(3) is not correct: "//trim(dimnames(3))//"")
+
+   !< Get the name of the unlimited dimension
+   call get_unlimited_dimension_name(fileobj, unlimit_dimname)
+   if (trim(unlimit_dimname) .ne. "dim2") &
+       call mpp_error(FATAL, "unlimited dimension name is not correct")
+
+   !< Get the dimensions
+   call get_dimension_size(fileobj, dimnames(1), dim_size)
+   if (dim_size .ne. 1) call mpp_error(FATAL, "dim1 was not read correctly")
+
+   call get_dimension_size(fileobj, dimnames(2), dim_size)
+   if (dim_size .ne. 0) call mpp_error(FATAL, "dim2 was not read correctly")
+
+   call get_dimension_size(fileobj, dimnames(3), dim_size)
+   if (dim_size .ne. sum(all_pelist+1)) call mpp_error(FATAL, "dim3 was not read correctly")
 
    call close_file(fileobj)
 else
