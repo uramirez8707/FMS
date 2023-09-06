57972227b5c44b811f7bcf908693c1f34db297af
Uriel Ramirez <uramirez8707@gmail.com>
2023-09-06 14:54:59 -0400

    Pass in the default missing_value to the math functions

--- diag_manager/fms_diag_object.F90
+++ diag_manager/fms_diag_object.F90
@@ -795,6 +795,14 @@ function fms_diag_do_reduction(this, field_data, diag_field_id, oor_mask, weight
       call mpp_error(FATAl, "The missing value for the field:"//trim(field_ptr%get_varname())//&
         &" was not allocated to the correct type. This shouldn't have happened")
     end select
+  else
+    select type (missing_val => get_default_missing_value(r8))
+    type is (real(kind=r8_kind))
+      missing_value = missing_val
+    class default
+      call mpp_error(FATAl, "The missing value for the field:"//trim(field_ptr%get_varname())//&
+        &" was not allocated to the correct type. This shouldn't have happened")
+    end select
   endif
 
   buffer_loop: do ids = 1, size(field_ptr%buffer_ids)
