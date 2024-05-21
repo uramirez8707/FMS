9330d2d59e5f216d4f8862c572814b253470ce04
Uriel Ramirez <uramirez8707@gmail.com>
2024-05-21 16:28:55 -0400

    small changes to reproduce the issue

--- test_fms/data_override/test_data_override_LOW.F90
+++ test_fms/data_override/test_data_override_LOW.F90
@@ -26,7 +26,7 @@ program test
 	real, allocatable :: outdata(:,:)
 	logical :: override
 	real, dimension(:), allocatable :: x, y
-	integer :: id_x, id_y, id_psl, id_tas, id_huss, id_uas, id_vas, id_rlds,id_rsds, id_prra, id_prsn, id_mole
+	integer :: id_x, id_y, id_rlds, id_rlds2, id_rlds3
 	logical :: used
   integer :: i
 
@@ -40,76 +40,41 @@ program test
 
 	!< Create a domain nlonXnlat with mask
 	call mpp_domains_set_stack_size(17280000)
-	call mpp_define_domains( (/1,nlon,1,nlat/), layout, Domain, xhalo=nhalox, yhalo=nhaloy, name='test_data_override_emc')
+	call mpp_define_domains( (/1,nlon,1,nlat/), layout, Domain, xhalo=nhalox, yhalo=nhaloy, symmetry=.false., name='test_data_override_emc')
 	call mpp_define_io_domain(Domain, (/1,1/))
 	call mpp_get_compute_domain(Domain, is, ie, js, je)
 
 	allocate(outdata(is:ie, js:je))
 
-	call data_override_init(atm_domain_in=Domain)
+	call data_override_init(ocean_domain_in=Domain)
+  call data_override_init(ice_domain_in=Domain)
+  call data_override_init(atm_domain_in=Domain)
+
 	call diag_manager_init()
 	Time = set_date(1993,1,1,0,0,0)
   allocate(x(540), y(696))
   call get_x_y(x,y)
 	id_y  = diag_axis_init('lat',  y,  units='degrees_east', cart_name='y', long_name='point_E', Domain2=Domain)
 	id_x  = diag_axis_init('lon',  x,  units='degrees_north', cart_name='x', long_name='point_N', Domain2=Domain)
-	id_psl = register_diag_field  ('ATM', 'psl', (/id_x,id_y/), Time, missing_value=-999.99)
-  id_tas = register_diag_field  ('ATM', 'tas', (/id_x,id_y/), Time, missing_value=-999.99)
-  id_huss = register_diag_field  ('ATM', 'huss', (/id_x,id_y/), Time, missing_value=-999.99)
-  id_uas = register_diag_field  ('ATM', 'uas', (/id_x,id_y/), Time, missing_value=-999.99)
-  id_vas = register_diag_field  ('ATM', 'vas', (/id_x,id_y/), Time, missing_value=-999.99)
 
-  id_rlds = register_diag_field  ('ATM', 'rlds', (/id_x,id_y/), Time, missing_value=-999.99)
-  id_rsds = register_diag_field  ('ATM', 'rsds', (/id_x,id_y/), Time, missing_value=-999.99)
-  id_prra = register_diag_field  ('ATM', 'prra', (/id_x,id_y/), Time, missing_value=-999.99)
-  id_prsn = register_diag_field  ('ATM', 'prsn', (/id_x,id_y/), Time, missing_value=-999.99)
-
-  id_mole = register_diag_field  ('ATM', 'mole_fraction_of_carbon_dioxide_in_air', (/id_x,id_y/), Time, missing_value=-999.99)
+  id_rlds = register_diag_field  ('OCN', 'rlds', (/id_x,id_y/), Time, missing_value=-999.99)
+  id_rlds2 = register_diag_field  ('OCN', 'rlds2', (/id_x,id_y/), Time, missing_value=-999.99)
+  id_rlds3 = register_diag_field  ('OCN', 'rlds3', (/id_x,id_y/), Time, missing_value=-999.99)
 
   Time = set_date(1993,1,1,0,0,0)
   call diag_manager_set_time_end(set_date(1994,1,1,0,0,0))
   do i = 1,31
     Time = Time + set_time (3600*24,0)
-  
-  outdata = -999.99
-  call data_override('ATM','psl',outdata, Time, override=override)
-	used = send_data(id_psl, outdata, time )
 
   outdata = -999.99
-  call data_override('ATM','tas',outdata, Time, override=override)
-	used = send_data(id_tas, outdata, time )
-
-  outdata = -999.99
-  call data_override('ATM','huss',outdata, Time, override=override)
-	used = send_data(id_huss, outdata, time )
-
-  outdata = -999.99
-  call data_override('ATM','uas',outdata, Time, override=override)
-	used = send_data(id_uas, outdata, time )
-
-  outdata = -999.99
-  call data_override('ATM','vas',outdata, Time, override=override)
-	used = send_data(id_vas, outdata, time )
-
-  outdata = -999.99
-  call data_override('ATM','rlds',outdata, Time, override=override)
+  call data_override('OCN','rlds',outdata, Time, override=override)
 	used = send_data(id_rlds, outdata, time )
 
-  outdata = -999.99
-  call data_override('ATM','prra',outdata, Time, override=override)
-	used = send_data(id_prra, outdata, time )
+  call data_override('ICE','rlds2',outdata, Time, override=override)
+	used = send_data(id_rlds2, outdata, time )
 
-  outdata = -999.99
-  call data_override('ATM','rsds',outdata, Time, override=override)
-	used = send_data(id_rsds, outdata, time )
-
-  outdata = -999.99
-  call data_override('ATM','prsn',outdata, Time, override=override)
-	used = send_data(id_prsn, outdata, time )
-
-  outdata = -999.99
-  call data_override('ATM','mole_fraction_of_carbon_dioxide_in_air',outdata, Time, override=override)
-	used = send_data(id_mole, outdata, time )
+  call data_override('ATM','rlds3',outdata, Time, override=override)
+	used = send_data(id_rlds3, outdata, time )
 
   call diag_send_complete(set_time (3600*24,0))
 enddo
